88c0fc796730dd985a9b72f76819bb5f91390e2f
Mel Maki <melody.rackham@gmail.com>
2020-03-02 15:17:32 -0700

    Initial commit - created react app, updated title, etc

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <p>Coming Soon -- Calm Massage Homepage!!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
