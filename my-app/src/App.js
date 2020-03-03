222db3297645568319509dbc3923ad05808bb0ae
Mel Maki <melody.rackham@gmail.com>
2020-03-03 10:16:09 -0700

    Updated app.js and index.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
-
 import './App.css';
+import Home from './components/Home';
+import About from './components/About';
+import Services from './components/Services';
+import Contact from './components/Contact';
+
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <p>Coming Soon -- Calm Massage Homepage!!</p>
-      </header>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/about' component={About} />
+        <Route exact path='/services' component={Services} />
+        <Route path='/contact' component={Contact} />
+      </Switch>
     </div>
   );
 }
